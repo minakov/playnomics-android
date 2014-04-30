4b683928f23b7b39d02cea064e16b2c93b9cfa1e
Viraf Zack <viraf@playnomics.com>
2014-04-30 16:46:36 -0700

    Fixed AsyncTaskRunner to always invoke execute on Main Thread

--- PlaynomicsSDK/src/com/playnomics/android/util/AsyncTaskRunner.java
+++ PlaynomicsSDK/src/com/playnomics/android/util/AsyncTaskRunner.java
@@ -1,25 +1,49 @@
 package com.playnomics.android.util;
 
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 
 import com.playnomics.android.util.IAsyncCall;
 
-public class AsyncTaskRunner  extends AsyncTask<String, String, String> {
+public class AsyncTaskRunner {
 	private IAsyncCall asyncCall = null;
+	private InternalAsyncTask asyncTask = null;
 
 	public AsyncTaskRunner(final IAsyncCall asyncCall) {
-		super();
 		this.asyncCall = asyncCall;
+		this.asyncTask = new InternalAsyncTask();
 	}
 
-	@Override
-	protected String doInBackground(String... arg0) {
-		asyncCall.onBackgroundThread();
-		return null;
+	public void execute() {
+		if (Looper.myLooper() != Looper.getMainLooper()) {
+			Handler handler = new Handler(Looper.getMainLooper()) {
+				@Override
+				public void handleMessage(Message msg) {
+					asyncTask.execute();
+				}
+			};
+			handler.sendEmptyMessage(0);
+			return;
+		}
+		asyncTask.execute();
 	}
-	
-	@Override
-	protected void onPostExecute(String s) {
-		asyncCall.postExecuteOnUiThread();
+
+	public void cancel(boolean mayInterruptIfRunning) {
+		asyncTask.cancel(mayInterruptIfRunning);
+	}
+
+	private class InternalAsyncTask extends AsyncTask<String, String, String> {
+		@Override
+		protected String doInBackground(String... arg0) {
+			asyncCall.onBackgroundThread();
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(String s) {
+			asyncCall.postExecuteOnUiThread();
+		}
 	}
 }
